892e40d828cad04f2713cb725299e0fe1154c2e6
watagashi0619 <watagashi0619@gmail.com>
2023-06-11 00:48:54 +0900

    3. recording to a csv file

--- adc_monitor/adc_monitor_main.c
+++ adc_monitor/adc_monitor_main.c
@@ -1,19 +1,24 @@
-#include <errno.h>         // error用
-#include <fcntl.h>         // ファイルディスクリプタfdの操作。O_RDONLYとかの定義も。
-#include <nuttx/config.h>  // config読み込み
-#include <stdio.h>         // 標準入出力。printfとか
+#include <errno.h>          // error用
+#include <fcntl.h>          // ファイルディスクリプタfdの操作。O_RDONLYとかの定義も。
+#include <nuttx/config.h>   // config読み込み
+#include <stdio.h>          // 標準入出力。printfとか
 #include <stdlib.h>
-#include <sys/boardctl.h>  // boardctl用。
-#include <sys/ioctl.h>     // ioctl、電圧受取部の制御に必要。
-#include <unistd.h>        // unix標準入出力。readとか。
+#include <sys/boardctl.h>   // boardctl用。
+#include <sys/ioctl.h>      // ioctl、電圧受取部の制御に必要。
+#include <unistd.h>         // unix標準入出力。readとか。
 
 #ifdef CONFIG_CXD56_ADC     // CONFIG_CXD56_ADCが宣言されている場合にブロック内を実行
 #include <arch/chip/adc.h>  // ANIOC_CXD56_START,STOP
 #include <arch/chip/scu.h>  // SCUIOC_SETFIFOMODE
 #endif
 
-#define BUFSIZE 8192               // ADC値をよむfdに一度にどれだけ読ませるか？
-#define ADC_DEVPATH "/dev/hpadc0"  // 読ませるADCピンの指定
+#define BUFSIZE 8192                 // ADC値をよむfdに一度にどれだけ読ませるか？
+#define ADC_DEVPATH "/dev/hpadc0"    // 読ませるADCピンの指定
+
+#define CSVFILE_ROOTPATH "/mnt/sd0"  // SDカードのルートパス
+#define MAX_PATH_LENGTH 128
+static char csvfname[MAX_PATH_LENGTH];
+static FILE *fd_csv;
 
 char buf[BUFSIZE];
 int fd;
@@ -55,6 +60,23 @@ _err:
     return;
 }
 
+// csvファイルをオープン
+static void open_csv() {
+    snprintf(csvfname, MAX_PATH_LENGTH, "%s/%s", CSVFILE_ROOTPATH, "log.csv");  // オープンするcsvファイルの名前を設定
+    fd_csv = fopen(csvfname, "a+");                                             // とりあえずひたすら追記するとしてa+を設定
+    if(fd_csv == 0) {
+        printf("open err(%s)\n", csvfname);
+        goto _err;
+    }
+_err:
+    return;
+}
+
+// csvファイルを閉じる
+static void close_csv() {
+    fclose(fd_csv);
+}
+
 // loop
 void loop() {
     int counter = 0;
@@ -75,7 +97,7 @@ void loop() {
         for(;;) {
             sample = (int16_t)(*(uint16_t *)(start));  // sampleされた電圧の生値を格納
             start += sizeof(uint16_t);
-            if(start >= end) {  // endにいくまで配列の値を読み続ける
+            if(start >= end) {                         // endにいくまで配列の値を読み続ける
                 break;
             }
             if(diff > threshold) {  // 記録開始地点を設定
@@ -83,9 +105,13 @@ void loop() {
                 flag = true;
             }
             if(flag && (counter == (entrypoint + interval / 2) % BUFSIZE)) {
-                for(int i = 0; i < interval; i++) {
-                    printf("%d\n", logs[(i + entrypoint - interval / 2) % BUFSIZE]);
+                open_csv();                                                                   // csvをオープン
+                fprintf(fd_csv, "%d", logs[(0 + entrypoint - interval / 2) % BUFSIZE]);       // csvファイルに書き込み
+                for(int i = 1; i < interval; i++) {
+                    fprintf(fd_csv, ",%d", logs[(i + entrypoint - interval / 2) % BUFSIZE]);  // csvファイルに書き込み
                 }
+                fprintf(fd_csv, "\n");
+                close_csv();  // csvをクローズ
                 flag = false;
             }
             counter++;
@@ -99,7 +125,7 @@ void loop() {
 int main(int argc, FAR char *argv[]) {
     boardctl(BOARDIOC_INIT, 0);  // 本体の電源を入れるとこのアプリケーションが走るようにするための設定
 
-    start_adc();  // start ADC
+    start_adc();                 // start ADC
 
     loop();
 
